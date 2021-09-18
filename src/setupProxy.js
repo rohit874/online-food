1afa8a8326a0d459079ec766c2e6875201d39d66
Rohit Kumar <rohitk9908@gmail.com.com>
2021-09-18 20:03:47 +0530

    one more commit

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,11 @@
-const proxy = require("http-proxy-middleware");
 
-module.exports = app => {
-  app.use(proxy("/api/*", { target: "https://online-food-backend-api.herokuapp.com" }));
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://online-food-backend-api.herokuapp.com',
+      changeOrigin: true,
+    })
+  );
 };
